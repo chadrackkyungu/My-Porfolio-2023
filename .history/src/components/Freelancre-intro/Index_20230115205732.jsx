1295cb2fe96a693e543d0df915d79ed3742163f2
Chadrack kyungu <65617666+chadrackkyungu@users.noreply.github.com>
2023-01-15 23:50:22 +0200

    Cleaned the projects

---
+++ .history/src/components/Freelancre-intro/Index_20230115205732.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import HomeSection from './components/HomeSection';
+
+function Index() {
+  return <HomeSection />
+}
+
+export default Index
---
+++ src/components/Work-page/Index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ProjectBox from './components/ProjectBox'
+
+function Index() {
+  return <ProjectBox />
+}
+
+export default Index
